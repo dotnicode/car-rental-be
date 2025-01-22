c762d9a3d257abb0e8fd32ac986e505609b7623d
dotnicode <romero.nico18@gmail.com>
2025-01-22 17:59:56 +0000

    refactor: enhance app logger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger, Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const logger = new Logger('Main');
+
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: 'CarRentalApp',
+    }),
+  });
+
   app.enableCors();
   app.setGlobalPrefix('api');
-  
-  await app.listen(process.env.PORT ?? 4000);
+
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port, () => {
+    logger.log(`Server is running on port ${port}`);
+  });
 }
 bootstrap();
