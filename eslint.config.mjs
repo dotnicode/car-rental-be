9953713dd851a7a583a8a7af6edb21ff3f292de5
dotnicode <romero.nico18@gmail.com>
2025-01-25 12:41:52 +0000

    chore: update eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  // eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -27,10 +27,11 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },  
+    },
   },
 );
+
