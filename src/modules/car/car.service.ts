26ad2eb774b5b231abfa25ae85cb32cd9da23842
dotnicode <romero.nico18@gmail.com>
2025-01-27 20:18:40 +0000

    refactor: reorganize car module providers and update service dependencies

--- src/modules/car/car.service.ts
+++ src/modules/car/car.service.ts
@@ -3,17 +3,22 @@ import { Repository } from 'typeorm';
 import { Inject, Injectable } from '@nestjs/common';
 
 import { DatabaseException } from '../../common/exceptions/database.exception';
-import { CAR_REPOSITORY } from './car.provider';
+import { CAR_REPOSITORY } from './providers/car.provider';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
 import { Car } from './entities/car.entity';
 import { CarNotFoundException } from './exceptions/car-not-found.exception';
+import { Picture } from './entities/picture.entity';
+import { PICTURE_REPOSITORY } from './providers/picture.provider';
 
 @Injectable()
 export class CarService {
   constructor(
     @Inject(CAR_REPOSITORY)
     private readonly carRepository: Repository<Car>,
+
+    @Inject(PICTURE_REPOSITORY)
+    private readonly pictureRepository: Repository<Picture>,
   ) {}
 
   async create(createCarDto: CreateCarDto) {
