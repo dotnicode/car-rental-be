1ae5a9c719db2a386f4bc086533eff53d0cdac3b
dotnicode <romero.nico18@gmail.com>
2025-01-29 21:06:34 +0000

    feat: add User module with basic CRUD operations
    - Create UserModule, UserController, and UserService
    - Add basic CRUD methods for user management
    - Integrate UserModule into the main AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CarModule } from './modules/car/car.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [DatabaseModule, CarModule],
+  imports: [DatabaseModule, CarModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
