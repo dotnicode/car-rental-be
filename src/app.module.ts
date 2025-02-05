0d9fb47c706bf96d7cf230485f60d1679d1fedca
dotnicode <romero.nico18@gmail.com>
2025-02-05 04:01:05 +0000

    refactor: remove default AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CarModule } from './modules/car/car.module';
 import { UtilsModule } from './modules/utils/utils.module';
 
 @Module({
   imports: [DatabaseModule, CarModule, UtilsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
